0cafda2d90f28593b477f7b5c0f2aabda6d74dc1
joaoPjunior <joaoomenordacasa@gmail.com>
2025-04-24 09:07:47 -0300

    melhoriasdocodigo

--- 01_metodos/script.js
+++ 01_metodos/script.js
@@ -1,11 +1,11 @@
-const cachorro = {
-    uivas: function(){
-        console.log("auuuuuuuuuu")
-    },
-    rosnar: function(){
-        console.log("grrrrrr")
+const pessoa = {
+    Nome: "João Paulo",
+    Idade: 31,
+    Profissão:"Programador",
+    Sexo: "Masculino",
+    saudacao: function(){
+        console.log(`Olá meu nome é ${this.Nome}, tenho a idade de ${this.Idade} anos, trabalho como ${this.Profissão}, e me identifico como sexo ${this.Sexo}.`);
     }
 }
 
-cachorro.uivas();
-cachorro.rosnar();
+pessoa.saudacao()

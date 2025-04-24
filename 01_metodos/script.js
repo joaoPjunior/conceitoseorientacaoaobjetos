3bfe10f2cd468fb7d03156a69ebeb131f11623b2
joaoPjunior <joaoomenordacasa@gmail.com>
2025-04-24 08:44:08 -0300

    primeirocommit

---
+++ 01_metodos/script.js
@@ -0,0 +1,11 @@
+const cachorro = {
+    uivas: function(){
+        console.log("auuuuuuuuuu")
+    },
+    rosnar: function(){
+        console.log("grrrrrr")
+    }
+}
+
+cachorro.uivas();
+cachorro.rosnar();

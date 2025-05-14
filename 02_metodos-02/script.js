e6b932ddaf0e4b8c1dcafa1985942214ac7a0138
joaoPjunior <joaoomenordacasa@gmail.com>
2025-05-14 11:01:13 -0300

    commitHoje

--- 02_metodos-02/script.js
+++ 02_metodos-02/script.js
@@ -1,12 +1,22 @@
 const cachorro = {
+    nome: "SND",
+    latir: function(){
+        console.log("AU AU AU AU AU");
+    },
     uivar: function(){
-        console.log('AUUUUUUUU');
+        console.log("AUUUUUUUUUUUUUUUU!");
     },
     rosnar: function(){
-        console.log('GRRRRRRRRRRRRRR');
+        console.log("GRRRRRRRRRRRRRRR!");
+    },
+    setNome: function(nome){
+        this.nome = nome;
     }
 }
 
 
+cachorro.latir();
 cachorro.uivar();
 cachorro.rosnar();
+cachorro.nome = "Bidu";
+console.log(cachorro.nome);
